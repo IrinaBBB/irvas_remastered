bc54c60317dd0e9a409013261a6679492c550a00
Irina <balkarovairina@gmail.com>
2021-07-25 12:03:31 +0200

    fix: modal window function

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,36 +1,39 @@
 const modals = () => {
-    function bindModal(trigger, modal, close) {
-        trigger.addEventListener('click', (e) => {
-            if (e.target) {
-                e.preventDefault();
-            }
+    function bindModal(triggerSelector, modalSelector, closeSelector) {
 
-            modal.style.display = 'block';
-            // document.body.style.overflow = 'hidden';
-            document.body.classList.add('modal-open');
+        const trigger = document.querySelectorAll(triggerSelector),
+            modal = document.querySelector(modalSelector),
+            close = document.querySelector(closeSelector);
 
-            close.addEventListener('click', () => {
-               modal.style.display = "none";
-               // document.body.style.overflow = '';
-                document.body.classList.remove('modal-open');
-
-            });
-
-            modal.addEventListener('click', (e) => {
-                if (e.target === modal) {
-                    modal.style.display = "none";
-                    // document.body.style.overflow = '';
-                    document.body.classList.remove('modal-open');
+        trigger.forEach(item => {
+            item.addEventListener('click', e => {
+                if (e.target) {
+                    e.preventDefault();
                 }
+                modal.style.display = 'block';
+                document.body.classList.add('modal-open');
+                // document.body.style.overflow = 'hidden';
             });
+        });
+
+        close.addEventListener('click', () => {
+           modal.style.display = "none";
+           // document.body.style.overflow = '';
+            document.body.classList.remove('modal-open');
+
+        });
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                modal.style.display = "none";
+                // document.body.style.overflow = '';
+                document.body.classList.remove('modal-open');
+            }
         });
     }
 
-    const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
-        modelEngineer = document.querySelector('.popup_engineer'),
-        modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
-
-    bindModal(callEngineerBtn, modelEngineer, modalEngineerClose);
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
 };
 
 export default modals;
